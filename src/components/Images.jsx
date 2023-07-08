3e9501b005e6c1ecafe35f5ae776a32b883bf4e0
mhyang721 <112519429+mhyang721@users.noreply.github.com>
2023-07-08 16:38:34 -0700

    Create images component

---
+++ src/components/Images.jsx
@@ -0,0 +1,17 @@
+const Images = () => {
+    return (
+        <>
+            <h1 className="text-center mt-6 underline text-2xl">Results for Cats</h1>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
+                <h1>Image 1</h1>
+                <h1>Image 1</h1>
+                <h1>Image 1</h1>
+                <h1>Image 1</h1>
+                <h1>Image 1</h1>
+                <h1>Image 1</h1>
+            </div>
+        </>
+    );
+  }
+  
+  export default Images;
